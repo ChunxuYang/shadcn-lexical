1ff0bcd53937a905ce6b22c6d7669972547162e6
Chunxu Yang <chunxuyang@ucla.edu>
2024-04-10 22:28:47 -0700

    feat: undo and redo

--- src/components/editor/plugins/toolbar-plugin.tsx
+++ src/components/editor/plugins/toolbar-plugin.tsx
@@ -1,25 +1,24 @@
 import {
-  $getSelection,
-  $isRangeSelection,
-  COMMAND_PRIORITY_CRITICAL,
-  FORMAT_TEXT_COMMAND,
-  SELECTION_CHANGE_COMMAND,
-} from "lexical";
-import { useCallback, useEffect, useState } from "react";
+    $getSelection, $isRangeSelection, CAN_REDO_COMMAND, CAN_UNDO_COMMAND, COMMAND_PRIORITY_CRITICAL,
+    FORMAT_TEXT_COMMAND, REDO_COMMAND, SELECTION_CHANGE_COMMAND, UNDO_COMMAND
+} from 'lexical';
+import { useCallback, useEffect, useState } from 'react';
 
-import { Toggle } from "@/components/ui/toggle";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import {
-  FontBoldIcon,
-  FontItalicIcon,
-  UnderlineIcon,
-} from "@radix-ui/react-icons";
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Toggle } from '@/components/ui/toggle';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { mergeRegister } from '@lexical/utils';
+import { FontBoldIcon, FontItalicIcon, ReloadIcon, UnderlineIcon } from '@radix-ui/react-icons';
 
 export default function ToolbarPlugin() {
   const [editor] = useLexicalComposerContext();
   const [isBold, setIsBold] = useState<boolean>(false);
   const [isItalic, setIsItalic] = useState<boolean>(false);
   const [isUnderline, setIsUnderline] = useState<boolean>(false);
+
+  const [canUndo, setCanUndo] = useState<boolean>(false);
+  const [canRedo, setCanRedo] = useState<boolean>(false);
 
   const $updateToolbar = useCallback(() => {
     const selection = $getSelection();
@@ -31,19 +30,68 @@ export default function ToolbarPlugin() {
   }, []);
 
   useEffect(() => {
-    return editor.registerCommand(
-      SELECTION_CHANGE_COMMAND,
-      () => {
-        $updateToolbar();
-        return false;
-      },
-      COMMAND_PRIORITY_CRITICAL
+    return mergeRegister(
+      editor.registerCommand(
+        SELECTION_CHANGE_COMMAND,
+        () => {
+          $updateToolbar();
+          return false;
+        },
+        COMMAND_PRIORITY_CRITICAL
+      ),
+      editor.registerUpdateListener(({ editorState }) => {
+        editorState.read(() => {
+          $updateToolbar();
+        });
+      })
     );
   }, [editor, $updateToolbar]);
 
+  useEffect(() => {
+    return mergeRegister(
+      editor.registerCommand(
+        CAN_UNDO_COMMAND,
+        (payload) => {
+          setCanUndo(payload);
+          return false;
+        },
+        COMMAND_PRIORITY_CRITICAL
+      ),
+      editor.registerCommand(
+        CAN_REDO_COMMAND,
+        (payload) => {
+          setCanRedo(payload);
+          return false;
+        },
+        COMMAND_PRIORITY_CRITICAL
+      )
+    );
+  }, [editor]);
+
   return (
-    <div className="w-full p-1 border-b z-10">
-      <div className="flex space-x-2 justify-center">
+    <div className="w-full border-b z-10 relative">
+      <div className="flex space-x-2 justify-center p-1">
+        <Button
+          className="h-8 px-2"
+          variant={"ghost"}
+          disabled={!canUndo}
+          onClick={() => editor.dispatchCommand(UNDO_COMMAND, undefined)}
+        >
+          {/* reload flip to left */}
+          <ReloadIcon className="transform -scale-x-100" />
+        </Button>
+
+        <Button
+          className="h-8 px-2"
+          variant={"ghost"}
+          disabled={!canRedo}
+          onClick={() => editor.dispatchCommand(REDO_COMMAND, undefined)}
+        >
+          <ReloadIcon />
+        </Button>
+
+        <Separator orientation="vertical" className="h-auto my-1" />
+
         <Toggle
           area-label="Bold"
           size="sm"
